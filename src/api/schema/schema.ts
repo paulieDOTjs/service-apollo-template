2efddb491e8ce771bb7cc233713ae61b9b3c2851
Paul Hendrickson <paulrobhendrickson@gmail.com>
2021-07-19 12:29:20 -0500

    Update schema.ts

--- src/api/schema/schema.ts
+++ src/api/schema/schema.ts
@@ -1,10 +1,10 @@
-import { makeExecutableSchema } from "apollo-server-express";
+import { buildFederatedSchema } from "@apollo/federation";
 import { typeDefs } from "./typeDefs";
 import { resolvers } from "./resolvers";
 
 // This takes in all type defs from the array in /typeDefs/typeDefs
 // and the resolvers from the object in ./resolvers/resolvers
-export const schema = makeExecutableSchema({
+export const schema = buildFederatedSchema({
   typeDefs,
   resolvers,
 });
